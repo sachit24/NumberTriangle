03b085cd5fc5eed0866948105cbf717e481f33e6
sachit24 <sachitsapra@gmail.com>
2025-09-27 18:58:29 -0400

    added retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,10 +88,16 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        if (path.isEmpty()) {
+            return this.root;
+        }
+        else if (path.charAt(0)=='l'){
+            return this.left.retrieve(path);
+        }
+        else{
+            return this.right.retrieve(path);
+        }
     }
-
     /** Read in the NumberTriangle structure from a file.
      *
      * You may assume that it is a valid format with a height of at least 1,
